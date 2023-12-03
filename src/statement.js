9162a4a2d38185adc9c4e833889fbf3d3115d8dd
Jerry.Shen <shenjinglei2010@163.com>
2023-12-03 15:11:54 +0800

    enrich performance record

--- src/statement.js
+++ src/statement.js
@@ -1,8 +1,13 @@
 function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances;
+  statementData.performances = invoice.performances.map(enrichPerfoemance);
   return renderPlainText(statementData, plays);
+
+  function enrichPerfoemance(aPerformance) {
+    const result = Object.assign({}, aPerformance);
+    return result;
+  }
 }
 
 function renderPlainText(data, plays) {
