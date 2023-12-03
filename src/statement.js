cfed0fc2507729f0d014c74f5ea09f1ba25df837
Jerry.Shen <shenjinglei2010@163.com>
2023-12-03 08:48:53 +0800

    Extract Function

--- src/statement.js
+++ src/statement.js
@@ -9,14 +9,19 @@ function statement(invoice, plays) {
     } seats)\n`;
     totalAmount += amountFor(perf);
   }
-  let volumeCredits = 0;
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-  }
+  let volumeCredits = totalVolumeCredits();
 
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
+
+  function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+      volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
+  }
 
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
