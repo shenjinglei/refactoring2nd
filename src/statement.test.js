97363aad596e318072123f0ab797a516f5f454d5
Jerry.Shen <shenjinglei2010@163.com>
2023-12-03 23:37:05 +0800

    add htmlStatement and test

--- src/statement.test.js
+++ src/statement.test.js
@@ -1,15 +1,25 @@
-import statement from "./statement";
+import { statement, htmlStatement } from "./statement";
 import invoices from "./invoices";
 import plays from "./plays";
 
 test("statement test", async () => {
-  console.log("invoices", invoices);
-
   expect(statement(invoices, plays)).toBe(`Statement for BigCo
  Hamlet: $650.00 (55 seats)
  As You Like It: $580.00 (35 seats)
  Othello: $500.00 (40 seats)
 Amount owed is $1,730.00
 You earned 47 credits
+`);
+});
+
+test("htmlStatement test", async () => {
+  expect(htmlStatement(invoices, plays)).toBe(`<h1>Statement for BigCo</h1>
+<table>
+<tr><th>play</th><th>seats</th><th>cost</th></tr> <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>
+ <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>
+ <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>
+</table>
+<p>Amount owed is <em>$1,730.00</em></p>
+<p>You earned <em>47</em> credits</p>
 `);
 });
